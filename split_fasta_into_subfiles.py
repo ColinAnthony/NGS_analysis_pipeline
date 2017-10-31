0e761b27d6a77fe3fd817f09cba2fbc048507b09
colin.anthony <colin.anthony@uct.ac.za>
2017-10-31 12:13:08 +0200

    fixed bugs in split fasta into subfiles

--- split_fasta_into_subfiles.py
+++ split_fasta_into_subfiles.py
@@ -4,10 +4,43 @@ from __future__ import division
 import os
 import argparse
 import collections
-from Bio import SeqIO
-#
+from itertools import groupby
 
-__author__ = 'Colin Anthony'
+
+__author__ = 'colin.anthony'
+
+
+def py3_fasta_iter(fasta_name):
+    """
+    modified from Brent Pedersen: https://www.biostars.org/p/710/#1412
+    given a fasta file. yield tuples of header, sequence
+    """
+    fh = open(str(fasta_name), 'r')
+    faiter = (x[1] for x in groupby(fh, lambda line: line[0] == ">"))
+    for header in faiter:
+        # drop the ">"
+        header_str = header.__next__()[1:].strip()
+        # join all sequence lines to one.
+        seq = "".join(s.strip() for s in faiter.__next__())
+        yield (header_str, seq)
+
+
+def fasta_to_dct(file_name):
+    """
+    :param file_name: The fasta formatted file to read from.
+    :return: a dictionary of the contents of the file name given. Dictionary in the format:
+    {sequence_id: sequence_string, id_2: sequence_2, etc.}
+    """
+    dct = collections.defaultdict(str)
+    my_gen = py3_fasta_iter(file_name)
+    for k, v in my_gen:
+        new_key = k.replace(" ", "_")
+        if new_key in dct.keys():
+            print("Duplicate sequence ids found. Exiting")
+            raise KeyError("Duplicate sequence ids found")
+        dct[new_key] = str(v).replace("~", "_")
+
+    return dct
 
 
 def gethxb2(dict):
@@ -34,28 +67,35 @@ def gethxb2(dict):
 
 def main(infile, outpath, field):
 
-    alignment = SeqIO.parse(infile, "fasta")
-    d = collections.defaultdict(list)
-    hxb2_key, hxb2_seq = gethxb2(d)
+    # import the data and remove HXB2 from alignment
+    alignment_d = fasta_to_dct(infile)
+    hxb2_key, hxb2_seq = gethxb2(alignment_d)
     print(hxb2_key)
     if hxb2_key is None:
         print("no hxb2 to remove")
     elif hxb2_key is not None:
         print("removing hxb2")
-        del d[hxb2_key]
+        del alignment_d[hxb2_key]
 
-    # get unique field(s) for splitting by time point
-    for sequence_obj in alignment:
-        unique_field = sequence_obj.description.split("_")[0:field]
-        new_name = "_".join(unique_field) + "_sep.fasta"
-        out_file_name = os.path.join(outpath, new_name)
-        d[out_file_name].append(sequence_obj)
+    #adjust the field value for python zero indexing
+    adjust_field = field + 1
 
-    # write the grouped sequences to their outfiles
-    for out_file, seq_objs in d.items():
-        for seq_obj in seq_objs:
-            with open(out_file, 'a') as handle:
-                handle.write(">{0}\n{1}\n".format(seq_obj.name, str(seq_obj.seq)))
+    # sort time-point/sample sequence entries into dictionaries by the specified unique field
+    split_d = collections.defaultdict(dict)
+    for name, sequence in alignment_d.items():
+        unique_field = name.split("_")[0:adjust_field]
+        new_name = "_".join(unique_field)
+        split_d[new_name][name] = sequence
+
+    # write the grouped sequences to their separate outfiles
+    for group, group_d in split_d.items():
+        outfile = os.path.join(outpath, group, "sep.fasta")
+        if os.path.isfile(outfile):
+            print("{0} file already exists, overwriting file".format(outfile))
+            os.unlink(outfile)
+        for seq_name, seq in group_d.items():
+            with open(outfile, 'a') as handle:
+                handle.write(">{0}\n{1}\n".format(seq_name, seq))
 
 
 if __name__ == "__main__":
@@ -66,8 +106,9 @@ if __name__ == "__main__":
     parser.add_argument('-o', '--outpath', default=argparse.SUPPRESS, type=str,
                         help='The path to where the output file will be created', required=True)
     parser.add_argument('-f', '--field', type=int, default=4, required=False,
-                        help="The field that differentiates your samples/time points (use the last field if multiple."
-                             "(ie: 4 for 'CAP177_2000_004wpi_V3C4_GGGACTCTAGTG_28, or 2 for SVB008_SP_GGTAGTCTAGTG_231")
+                        help="The field that differentiates your samples/time points."
+                             "(ie: for the sequence name 'CAP177_2000_004wpi_V3C4_GGGACTCTAGTG_28_001_0.953,"
+                             " 1 = CAP177, 4 = CAP177_2000_004wpi_V3C4, etc...")
 
     args = parser.parse_args()
     infile = args.infile
