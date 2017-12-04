59c8e320fddcd7f9df17d41c23c809e139e8d1bc
colin.anthony <colin.anthony001@gmail.com>
2017-12-04 12:56:55 +0200

    added legend editing

--- plot_mulit_site_freq.py
+++ plot_mulit_site_freq.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 from __future__ import print_function
 from __future__ import division
 import argparse
@@ -8,12 +7,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 from matplotlib.ticker import AutoMinorLocator
-import matplotlib.cm as cm
-import numpy as np
-import matplotlib.patches as mpatches
-import matplotlib.lines as mlines
-from pprint import pprint
-
+import seaborn as sns
 
 __author__ = 'colin'
 
@@ -30,14 +24,6 @@ def transform_df_by_vl(headers, df, vl_file, participant):
     item = headers[1]
 
     test_df = df.copy(deep=True)
-    test_df.drop(['sequence_id'], inplace=True, axis=1)
-    ndf = test_df.groupby(time)[item].value_counts().reset_index(name='count')
-
-    total = test_df.groupby(time).agg({time: 'count'})
-    total.rename(columns={time: time, time: 'total'}, inplace=True)
-    total = total.reset_index()
-    df1 = ndf.merge(total[[time, 'total']], on=[time])
-    df1["frequency"] = ndf['count'] / df1['total'] * 100
 
     if vl_file is not None:
         # read in the viral load data
@@ -51,17 +37,16 @@ def transform_df_by_vl(headers, df, vl_file, participant):
         participant_vl['Time'] = participant_vl.loc[:, 'Time'].astype(int)
         participant_vl['viral_load'] = participant_vl.loc[:, 'viral_load'].astype(str).astype(int)
         participant_vl.drop(["participant_id"], inplace=True, axis=1)
-        df2 = df1.merge(participant_vl, on=[time])
-        df2["frequency"] = df2['count'] / df2['total'] * 100
-        df2['freq_viral_copies'] = (df2["frequency"] / 100) * df2['viral_load']
+        df2 = test_df.merge(participant_vl, on=[time])
+        df2['freq_viral_copies'] = df2["Frequency"] * df2['viral_load']
         df2.to_csv("wrangled_df.csv", sep=',')
 
         return df2
     else:
-        return df1
+        return test_df
 
 
-def divergence_plotter(headers, df, name, outpath, ab_time, bnab_time, av_heads, av_df, vl_file):
+def divergence_plotter(headers, df, name, outfile, ab_time, bnab_time, vl_file):
     """
     :param headers: x axis header
     :param item: y axis header
@@ -70,28 +55,16 @@ def divergence_plotter(headers, df, name, outpath, ab_time, bnab_time, av_heads,
     :param vl_file:
     :return: prints graphs to file
     """
-    x_header = headers[0]
-    y_header = headers[1]
 
-    n = y_header.replace(" ", "_")
-    if "_" in name:
-        title = name.replace("_", " ")
-    else:
-        title = name
-
-    outname = name + "_" + n + ".png"
-    outfile = os.path.join(outpath, outname)
-    print("outfile is:", outfile)
-
-    ymax = int(max(df[y_header]) * 1.05)
-    xmax = int(max(df[x_header]) + 20)
+    ymax = 101
+    xmax = 60
     ymin = 0
     xmin = 0
-    # df.sort_values(by=[y_header], inplace=True, ascending=True)
-    if vl_file is not None:
-        df.sort_values(by=["freq_viral_copies"], inplace=True, ascending=True)
-    else:
-        df.sort_values(by=["frequency"], inplace=True, ascending=True)
+    selection_bound = 10
+    x_step = int(xmax/10.0)
+    # fiter the dataframe to only those haplotypes which are greater than the % selection bound and up to time xmax
+    df = df[(df.Time <= xmax) & df.groupby("Haplotype")["Frequency"].filter(lambda x: x.max() > selection_bound)]
+
     # set axes
     fig, ax = plt.subplots(1, 1)
     ax.axes.get_yaxis().set_visible(True)
@@ -102,38 +75,19 @@ def divergence_plotter(headers, df, name, outpath, ab_time, bnab_time, av_heads,
     ax.get_xaxis().tick_bottom()
     ax.get_yaxis().tick_left()
     ax.set_facecolor('white')
+    #
+    df.set_index("Time", inplace=True)
+    df.groupby("Haplotype")["Frequency"].plot(legend=True)
+    plt.legend(frameon=False, framealpha=False,bbox_to_anchor=(1.01, 0.901))
 
-    # plot the data
-    if vl_file is None:
-        ax.scatter(df[x_header], df[y_header], alpha=0.6, s=df["frequency"]*20, edgecolor='black', lw=0.5, zorder=2)
-    else:
-        ax.scatter(df[x_header], df[y_header], alpha=0.6, s=df["freq_viral_copies"]/100, edgecolor='black', lw=0.5,
-                   zorder=2)
-
-    ## add legend
-    # c=df[item],
-    # add figure legend
-    # Maj_var_patch = mlines.Line2D([], [], color='#b15a29', marker='.',
-    #                       markersize=15, label='Major Variant')
-    # Min_var_patch = mlines.Line2D([], [], color='#97b9cc', marker='.',
-    #                               markersize=15, label='Minor Variant')
-    # plt.legend(handles=[Maj_var_patch, Min_var_patch], bbox_to_anchor=(1.1, 1.05), frameon=False)
-
-    # add average line
-    ax.scatter(av_df[av_heads[0]], av_df[av_heads[1]], alpha=1, s=10, c="#000000", lw=0.1, zorder=3)
-    plt.plot(av_df[av_heads[0]], av_df[av_heads[1]], linewidth=1.0, c="#000000", zorder=3)
-    plt.fill_between(av_df[av_heads[0]], av_df[av_heads[1]] - av_df[av_heads[2]], av_df[av_heads[1]] + av_df[av_heads[2]],
-                     color="#b2b5ba", alpha=1, zorder=1)
-
-    #plt.title(title, fontsize=18)
-    plt.ylim(ymin, ymax)
+    plt.ylim(ymin, ymax + 1)
     plt.xlim(xmin, xmax)
-    plt.yticks(list([x / 10 for x in range(int(ymin * 10), int(ymax * 10), 20)]), fontsize=14)
-    ax.get_yaxis().set_minor_locator(mpl.ticker.MultipleLocator(base=5))
+    plt.yticks(list(range(ymin, ymax, 20)), fontsize=14)
+    plt.xticks(list(range(xmin, xmax, x_step)), fontsize=14)
 
-    plt.ylabel("Divergence (%)", fontsize=24, labelpad=14)
+    plt.ylabel("Frequency (%)", fontsize=24, labelpad=14)
     plt.xlabel("Weeks post infection", fontsize=24, labelpad=12)
-    plt.xticks(list(range(xmin,xmax, 20)), fontsize=14)
+
 
     # add annotations for nAb time points
     if ab_time is not None:
@@ -148,7 +102,7 @@ def divergence_plotter(headers, df, name, outpath, ab_time, bnab_time, av_heads,
     h = 4
     f = plt.gcf()
     f.set_size_inches(w, h)
-
+    # plt.show()
     plt.savefig(outfile, ext='png', dpi=600, format='png', facecolor='white', bbox_inches='tight')
 
 
@@ -174,16 +128,17 @@ def main(infile, vl_file, name, ab_time, bnab_time, outpath):
     participant = name.split("_")[0]
 
     ndf = transform_df_by_vl(headers, df, vl_file, participant)
+    headers = list(ndf)
+    outfile = os.path.join(outpath, name + "_multi_site_site.png")
 
-    outfile1 = os.path.join(outpath, name + "_av_distance.csv")
-    sum_av_dist = df.groupby([headers[0]]).agg({headers[1]: ['mean', 'std']})
-    av_vals = pd.DataFrame(sum_av_dist.to_records())
-    av_heads = list(av_vals)
-    av_vals.rename(columns={av_heads[0]: av_heads[0], av_heads[1]: 'mean', av_heads[2]: 'std'}, inplace=True)
-    av_vals.to_csv(outfile1 + "_" + str(headers[1]) + "_av_loop_stats.csv", sep=",", index=False)
-    av_headers = list(av_vals)
+    # sum_av_dist = df.groupby([headers[0]]).agg({headers[1]: ['mean', 'std']})
+    # av_vals = pd.DataFrame(sum_av_dist.to_records())
+    # av_heads = list(av_vals)
+    # av_vals.rename(columns={av_heads[0]: av_heads[0], av_heads[1]: 'mean', av_heads[2]: 'std'}, inplace=True)
+    # av_vals.to_csv(outfile1 + "_" + str(headers[1]) + "_av_loop_stats.csv", sep=",", index=False)
+    # av_headers = list(av_vals)
 
-    divergence_plotter(headers, ndf, name, outpath, ab_time, bnab_time, av_headers, av_vals, vl_file)
+    divergence_plotter(headers, ndf, name, outfile, ab_time, bnab_time, vl_file)
 
 
 if __name__ == "__main__":
